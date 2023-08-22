90290f539d41761a8637f2f61cf552368c1c23e1
Zamatolo Mtolo <135616311+zamatolosnmtolo@users.noreply.github.com>
2023-08-22 21:28:55 +0200

    Update js

--- Challenge 2/scripts.js
+++ Challenge 2/scripts.js
@@ -1,36 +1,36 @@
-function add = a, b => { a + b }
-
-function multiply = a, b => { a - b }
+// Define the functions
+const add = (a, b) => { return a + b; }
+const multiply = (a, b) => { return a * b; }
 
 function internal() {
-	const added = this.add(this.a, this.b)
-	this.multiply(this.a, this.b)
-	return this
+  const added = this.add(this.internal.a, this.internal.b);
+  const multiplied = this.multiply(this.internal.a, this.internal.b);
+  return added + multiplied; // Return the sum of added and multiplied
 }
 
 // Not allowed to change below this
 
 const example1 = {
-	internal: {
-		a: 2,
-		b: 4,
-		c: 8,
-	},
-	add,
-	multiply,
+  internal: {
+    a: 2,
+    b: 4,
+    c: 8,
+  },
+  add,
+  multiply,
   calculate: internal
 }
 
 const example2 = {
-	internal: {
-		a: 2,
-		b: 2,
-		c: 3,
-	},
-	add,
-	multiply,
+  internal: {
+    a: 2,
+    b: 2,
+    c: 3,
+  },
+  add,
+  multiply,
   calculate: internal
 }
 
-example1.calculate()
-example2.calculate()
+console.log(example1.calculate()); // Should log 16 (8 + 8)
+console.log(example2.calculate()); // Should log 8 (4 + 4)
